84c5dcddcb8c8ac8a683206bd175ca42e6c23c44
wego <eguurt@mail.ru>
2017-11-25 18:59:03 +0300

    try to fix images

--- src/components/state/GameStore.js
+++ src/components/state/GameStore.js
@@ -1,10 +1,10 @@
 
 import { observable } from 'mobx';
-import skin1 from '../../images/skin1.png';
-import skin2 from '../../images/skin2.png';
-import level1 from '../../images/level1.png';
-import level2 from '../../images/level2.png';
-import level3 from '../../images/level3.png';
+import skin1 from './../../images/skin1.png';
+import skin2 from './../../images/skin2.png';
+import level1 from './../../images/level1.png';
+import level2 from './../../images/level2.png';
+import level3 from './../../images/level3.png';
 
 const game = observable({
     levels: [
