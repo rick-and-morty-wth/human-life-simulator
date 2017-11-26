2a0d0e8ea5a7bb3b5dcd088cd7515ced903a1e23
wego <eguurt@mail.ru>
2017-11-26 17:14:04 +0300

    some rappid fixes

--- src/components/pages/easterMovieGame.jsx
+++ src/components/pages/easterMovieGame.jsx
@@ -6,7 +6,13 @@ import {ContentBlock, Page} from 'framework7-react';
 import {inject, observer} from 'mobx-react';
 import {Link} from 'framework7-react';
 import HomeSmallButton from '../controls/HomeSmallButton/HomeSmallButton';
-export default inject("easterGame")(observer(({easterGame}) => {
+
+import {
+    Button
+} from 'framework7-react';
+
+
+export default inject("easterGame", "sounds")(observer(({easterGame, sounds}) => {
     return (
         <Page className="easter-movie-game-screen__wr">
         {easterGame.current ?
@@ -14,7 +20,7 @@ export default inject("easterGame")(observer(({easterGame}) => {
             <div className = 'easter-movie-game__header'>
                 <Header>
                     <div></div>
-                    <HomeSmallButton></HomeSmallButton>
+                    <HomeSmallButton onClick={sounds.play("btn")}></HomeSmallButton>
                 </Header>
             </div>
             <div className = 'qustiontDiv'>
@@ -29,7 +35,7 @@ export default inject("easterGame")(observer(({easterGame}) => {
                         <div className="answer__badge">A</div>
                         <button onClick={easterGame.next(easterGame.current.answer1, easterGame.current)}>{easterGame.current.answer1}</button>
                     </div>
-                    <div clasclassNames="answer__item">
+                    <div className="answer__item">
                         <div className="answer__badge">B</div>
                         <button onClick ={easterGame.next(easterGame.current.answer2, easterGame.current)}>{easterGame.current.answer2}</button>
                     </div>
@@ -40,13 +46,22 @@ export default inject("easterGame")(observer(({easterGame}) => {
                 </div>
             </div>
         </div> : <div>
-            <div className='result'>
-
-            Results TODO
-            <div class='gameScore'>{easterGame.score.length}</div>
-
+            <Header>
+                <HomeSmallButton onClick={sounds.play("btn")}></HomeSmallButton>
+            </Header>
+            <div className="store__shop-content">
+                <div className="store__shop-title">Hey brrrp mr. meeseeks, get shwifty.</div>
+                <div className="store__shop-sub-title">75%</div>
             </div>
-            <Link onClick={easterGame.reset()} back>Back</Link>
+            <div className="store__shop-wr">
+                <Button back className="store__shop-btn" onClick={()=> {
+                        sounds.play("btn")();
+                }
+                }>Done</Button>
+                <div className="store__shop-hint">
+                    find a next game idiot
+                </div>
+            </div>
         </div>}
         </Page>
     );
